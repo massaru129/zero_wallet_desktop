3d1957d11c809293668f5a5b086387a9a0ec8c18
masaru <massaru129@gmail.com>
2019-09-12 10:36:56 +0900

    send form 作成

--- src/renderer/components/settings.tsx
+++ src/renderer/components/settings.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 export default class Settings extends React.Component {
     public render() {
-        return <h2>Settings</h2>;
+        return (
+          <div>
+            <h2>Settings</h2>
+            <ul>
+              <li>Backup</li>
+              <li>Recovery</li>
+            </ul>
+          </div>
+        );
     }
 }
